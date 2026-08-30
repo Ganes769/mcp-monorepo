098c0a635b48664f77743a070597034618fb0209
Ganes769 <ganeshsnawali@gmail.com>
2026-08-30 22:17:01 +0100

    structured output

--- langchain-langraph/langchain/searchagent.py
+++ langchain-langraph/langchain/searchagent.py
@@ -1,13 +1,23 @@
 import os
+from typing import List
 
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
 from langchain_core.tools import tool
 from langchain_groq import ChatGroq
+from pydantic import BaseModel, Field
 from tavily import TavilyClient
 
 load_dotenv()
+class Source(BaseModel):
+    """Schema for source used by  agent"""
+    url:str=Field(description="The URL of the source")
+class AgentResponse(BaseModel):
+    """"Schema for agent response"""
+    ans:str=Field(description="Agent answer for schema")
+    sources:list[Source]=Field(default_factory=list,description="list of sources used to generate the asnwer")
+    better:str=Field(description="is this better for human being life?")
 
 tavily = TavilyClient(api_key=os.environ.get("TAVILY_API_KEY"))
 
@@ -19,10 +29,9 @@ def search(query: str) -> str:
     result = tavily.search(query=query, max_results=3)
     return str(result)
 
-
-llm = ChatGroq(model="qwen/qwen3.8-27b")
-agent = create_agent(model=llm, tools=[search])
+llm=ChatGroq(model="qwen/qwen3.8-27b")
+agent = create_agent(model=llm, tools=[search],response_format=AgentResponse)
 response = agent.invoke(
-    {"messages": [HumanMessage(content="who is better messi or ronaldo?")]}
+    {"messages": [HumanMessage(content="is ai is better for human?")]}
 )
 print(response)
